9bf0c34fbe3284dd1e9a27bf92b8397e1c64ed07
Alex Verschoot <alexverschoot@gmail.com>
2016-04-06 14:17:31 +0200

    Changed some names, added a basic action to the buttons.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "iostream"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_launcherStartButton_clicked()
+{
+    //TODO include some serial communication with the arduino
+    std::cout<<"launcher should now start"<<std::endl;
+}
+
+void MainWindow::on_launcherStopButton_clicked()
+{
+    //TODO include some serial communication with the arduino
+    std::cout<<"launcher should now stop"<<std::endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_launcherStartButton_clicked();
+
+    void on_launcherStopButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
